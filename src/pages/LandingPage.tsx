e8c07e45a7d7b1385800ca27f4543dbde68d514c
Dananuvindu <maduprabashwara@gmail.com>
2024-10-15 15:16:32 +0530

    ui updated

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import BackgroundImg from '../assets/background.png'
 import SignupAndLogin from "../components/landingPage/SignupAndLogin";
 import LanguageButton from "../components/landingPage/LanguageButton";
-import NavBar from "../components/NavBar";
+
 
 const LandingPage = () => {
   return (
@@ -10,7 +10,6 @@ const LandingPage = () => {
       className="w-screen h-screen bg-cover bg-no-repeat flex flex-col "
       style={{ backgroundImage: `url(${BackgroundImg})` }}
     >
-        <NavBar  topic="Price List"/>
         <SignupAndLogin/>
         <LanguageButton/>
     </div>
